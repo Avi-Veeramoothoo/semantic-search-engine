f29e458ffa6de8e7d029f1d05f469b7d5b8d7ce3
Avi <tveeramoothoo@gmail.com>
2023-12-26 19:46:37 -0500

    Semantic Search engine with vector search

---
+++ movieRecs.py
@@ -0,0 +1,34 @@
+import pymongo
+import requests
+
+client = pymongo.MongoClient("mongodb+srv://tveeramoothoo:AVIPASSWORD@cluster0.uueb0pj.mongodb.net/?retryWrites=true&w=majority")
+db = client.sample_mflix
+collection = db.movies
+
+hf_token = "AVITOKEN"
+embedding_url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
+
+
+def generate_embedding(text: str):
+    response = requests.post(
+        embedding_url,
+        headers={"Authorization": f"Bearer {hf_token}"},
+        json={"inputs": text})
+    if response.status_code != 200:
+        raise ValueError(f"Request failed with status code {response.status_code}: {response.text}")
+    return response.json()
+
+query = "imaginary characters from outer space at war"
+
+results = collection.aggregate([
+  {"$vectorSearch": {
+    "queryVector": generate_embedding(query),
+    "path": "plot_embedding_hf",
+    "numCandidates": 100,
+    "limit": 4,
+    "index": "PlotSemanticSearch",
+      }}
+]);
+
+for document in results:
+    print(f'Movie Name: {document["title"]},\nMovie Plot: {document["plot"]}\n')
